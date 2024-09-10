66da16d9d28fcc37f1e6c4dcd7cae3e0059733ee
devgabrielvieira <gabriel_bardo@hotmail.com>
2024-09-10 18:09:32 -0300

    adjustments and corrections for responsiveness

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,21 @@
 from django import forms
-from . models import Client
+from . import models
 
 
 class ClientForm(forms.ModelForm):
 
     class Meta:
-        model = Client
+        model = models.Client
         fields = ['nome_completo', 'email', 'telefone', 'cpf']
+        widgets = {
+            'nome_completo': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'rows': 3}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control', 'rows': 3}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control', 'rows': 3}),
+        }
+        labels = {
+            'nome_completo': 'Nome completo',
+            'email': 'Email',
+            'telefone': 'Telefone',
+            'cpf': 'CPF',
+        }
