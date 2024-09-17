b35e867e39ef099b5c38be1f9bc0f459ce433d01
devgabrielvieira <gabriel_bardo@hotmail.com>
2024-09-17 12:41:46 -0300

    update

--- onepage_project/settings.py
+++ onepage_project/settings.py
@@ -139,3 +139,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = 'ALLOWALL'
