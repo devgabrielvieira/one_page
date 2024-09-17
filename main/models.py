480488e33e8163772a2bfb16ccbbcc3f8504c30b
devgabrielvieira <gabriel_bardo@hotmail.com>
2024-09-17 13:45:06 -0300

    all

--- main/models.py
+++ main/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Client(models.Model):
     nome_completo = models.CharField(max_length=100)
     email = models.EmailField()
-    telefone = models.CharField(max_length=15)
-    cpf = models.CharField(max_length=14)
+    telefone = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11)
 
     def __str__(self):
         return self.nome_completo
